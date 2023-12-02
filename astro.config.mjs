2f330531336275807a828caab63c01f94474ca05
LordChebupelya <111064437+LordChebupelya@users.noreply.github.com>
2023-12-02 19:47:19 +0300

    major update: projects page, socials, etc. basically made everything fancy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 6969 },
+  prefetch: true,
+  server: {
+    port: 6969,
+  },
+  integrations: [tailwind()],
 });
